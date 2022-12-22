14e189c57ef1c0ccfdde6ae59ee71c3e1f8c4e2d
Christian Jaeger <ch@christianjaeger.ch>
2022-12-23 00:32:00 +0100

    Cleanup

--- src/item/char_util.rs
+++ src/item/char_util.rs
@@ -3,12 +3,12 @@ pub fn char_is_ascii_alphabetic(c: char) -> bool {
     c.is_ascii_alphabetic()
 }
 
-pub fn char_is_ascii_alphanumeric(c: char) -> bool {
-    c.is_ascii_alphanumeric()
-}
-
 pub fn char_is_ascii_digit(c: char) -> bool {
     c.is_ascii_digit()
+}
+
+pub fn char_is_ascii_alphanumeric(c: char) -> bool {
+    c.is_ascii_alphanumeric()
 }
 
 pub fn char_is_word(c: char) -> bool {
@@ -18,17 +18,4 @@ pub fn char_is_word(c: char) -> bool {
 pub fn char_is_whitespace(c: char) -> bool {
     c.is_whitespace() // is_ascii_whitespace ? XX
 }
-
-// fn char_is_not_underscore(c: char) -> bool {
-//     ! (c == '_')
-// }
-
-// fn len_is(s: &Colstr, l: usize) -> bool {
-//     s.str.len() == l
-// }
-
-// fn len_is_from_to(s: &Colstr, from: usize, to: usize) -> bool {
-//     let l = s.str.len();
-//     from <= l && l <= to
-// }
 
